72ea74b81470911705465b260767b6bdd3b3b110
Apple <apple@iMac-Apple.local>
2017-03-06 16:53:33 +0100

    multi-leagues adding

--- football_bets.py
+++ football_bets.py
@@ -2,35 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-LIGUES_ID = {
+LIGUES_TO_ID = {
     "premier league": "46",
     "la liga": "16108",
     "bundesliga": "43",
     "seria a": "42"
 }
 
+BASE_URL = "https://sports.bwin.com/en/sports/indexmultileague"
 
-#  r = requests.post('http://httpbin.org/post', data = {'key':'value'})
-# https://sports.bwin.com/en/sports/indexmultileague
 def get_ligue_parse_html(url):
-    r = requests.post(url, data={'sportId': '4',
-                                 'page': '0',
-                                 'leagueIds': [LIGUES_ID.values()]
-                                 })
+    r = requests.post(
+        url,
+        data={
+            'sportId': '4',
+            'page': '0',
+            'leagueIds': [LIGUES_TO_ID.values()]
+        })
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
     return soup
 
 
 def get_ligue_url(ligue):
-    return LIGUES_ID.get(ligue)
+    return LIGUES_TO_ID.get(ligue)
 
-
-x = "https://sports.bwin.com/en/sports/indexmultileague"
-soup = get_ligue_parse_html(x)
-
-
-# print(soup.prettify())
 
 def re_stake(stake):
     stake_word = re.findall(r"([\d.]*\d+)", stake)
@@ -38,13 +34,12 @@ def re_stake(stake):
 
 
 def get_local_bets(soup):
-    result = soup.find_all('div', {
-        'class': "marketboard-event-group__item-container marketboard-event-group__item-container--level-2"})
-    # print(len(result))
+    result = soup.find_all(
+        'div', {'class': "marketboard-event-group__item-container marketboard-event-group__item-container--level-2"})
     bets = []
     for z in range(len(result)):
         bet_rows = result[z].find_all('tr', {'class': "marketboard-options-row marketboard-options-row--3-way"})
-        # print(bet_rows)
+
         for row in bet_rows:
             choices = row.select('.mb-option-button__option-name')
             odds = row.select('.mb-option-button__option-odds')
@@ -64,14 +59,11 @@ def get_local_bets(soup):
             }
 
             bets.append(bet)
+    return bets
+
+
+if __name__ == '__main__':
+    soup = get_ligue_parse_html(BASE_URL)
+    bets = get_local_bets(soup)
     print(bets)
     print(len(bets))
-
-
-get_local_bets(soup)
-
-#
-# if __name__ == '__main__':
-#     soup = get_ligue_parse_html(get_ligue_url("la liga"))
-#     print(soup)
-#     get_local_bets(soup)
